ef067d1b788f3b75707481054f6e61f9c31a076e
oscarHuaripata <osduar27@gmail.com>
2021-09-10 20:42:46 +0000

    2

--- Poker/Poker.Tests/PokerJuegoTests.cs
+++ Poker/Poker.Tests/PokerJuegoTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,28 @@ namespace Poker.Tests
 {
     class PokerJuegoTests
     {
+
+        [Test]
+        public void Caso1() {
+
+            var game = new PokerGame();
+
+            var jugador = new Jugador { Id = 1, Nombre = "Oscar" };
+
+            game.RegistrarJugador(new Jugador { Id = 1, Nombre = "Oscar" });
+            game.RegistrarJugador(new Jugador { Id = 2, Nombre = "Eduardo" });
+
+            game.Lanzar(1); // jugada del jugador 1
+            game.Lanzar(1); // jugada del jugador 1
+            
+
+            var puntajeJugador1 = game.GetPuntajeJugador(1);
+            var puntajeJugador2 = game.GetPuntajeJugador(2);
+
+            Assert.AreEqual(2, puntajeJugador1);
+            Assert.AreEqual(2, puntajeJugador2);
+
+
+        }
     }
 }
--- Poker/Poker/PokerJuego.cs
+++ Poker/Poker/PokerJuego.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Poker
 {
-    class PokerJuego
-    {
+    
+        public class Jugador
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public int Puntaje { get; set; }
+        }
 
-
+       
+        }
     }
-}
+
+
+
+
+
